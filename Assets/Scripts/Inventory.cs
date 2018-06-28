63e8aba0227dc230e5151b69d263a5cf13ef5163
Lucas Rufino <lucaslrt@outlook.com>
2018-06-28 17:35:43 -0300

    Creating drag items.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -31,14 +31,32 @@ public class Inventory : MonoBehaviour {
 
     public void AddItem(int id) {
         Item itemToAdd = database.FetchItemById(id);
+
         for(int i = 0; i < items.Count; i++) {
             if(items[i].Id == -1) {
                 items[i] = itemToAdd;
                 GameObject itemObj = Instantiate(inventoryItem);
+                itemObj.GetComponent<ItemData>().item = itemToAdd;
                 itemObj.transform.SetParent(slots[i].transform);
                 itemObj.GetComponent<Image>().sprite = itemToAdd.Sprite;
                 itemObj.transform.position = Vector2.zero;
+
+                break;
+            } 
+            else if (items[i].Id == id) {
+                // Adicionar o som do item aqui.
+                ItemData data = slots[i].transform.GetChild(0).GetComponent<ItemData>();
+
+                break;
             }
         }
     }
+
+    /*void CheckIfItemIsInInventory(Item item) {
+        for(int i = 0; i < items.Count; i++) {
+            if(items[i].Id == item.Id) {
+
+            }
+        }
+    }*/
 }
---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    public Item item;
+    // TODO: make sound;
+
+    private Transform originalParent;
+    private Vector2 currentPosition;
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        if(item != null) {
+            originalParent = this.transform.parent;
+            this.transform.SetParent(this.transform.parent.parent);
+            this.transform.position = eventData.position;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        if (item != null) {
+            this.transform.position = eventData.position;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        this.transform.SetParent(originalParent);
+
+        this.transform.position = originalParent.transform.position;
+        currentPosition = this.transform.position;
+        //CreateNewItemOnCurrentPosition();
+    }
+
+    private void CreateNewItemOnCurrentPosition() {
+
+        GameObject newItem = Instantiate(new GameObject());
+        newItem.transform.position = currentPosition;
+        newItem.GetComponent<ItemData>().item = this.item;
+        newItem.GetComponent<Image>().sprite = this.item.Sprite;
+    }
+}
